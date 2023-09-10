4b4c47db909b20e85a3bbe3a72369d37a11ca1f0
housekjh1 <housekjh@gmail.com>
2023-09-10 20:42:08 +0900

    0910
    - eight queen 문제 풀이

---
+++ study_java_edu/src/Chap5_Recursive/Chap5_Test_QueenEight_4회차_01.java
@@ -0,0 +1,218 @@
+package Chap5_Recursive;
+
+class Point {
+	private int x, y;
+
+	public Point(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+}
+
+class Stack {
+	private Point[] data;
+	private int capacity, pt;
+
+	public class EmptyIntStackException extends RuntimeException {
+		public EmptyIntStackException() {
+		}
+	}
+
+	public class OverflowIntStackException extends RuntimeException {
+		public OverflowIntStackException() {
+		}
+	}
+
+	public Stack(int maxlen) {
+		this.capacity = maxlen;
+		this.pt = 0;
+		try {
+			this.data = new Point[capacity];
+		} catch (Exception e) {
+			this.capacity = 0;
+		}
+	}
+
+	public void push(Point p) throws OverflowIntStackException {
+		if (pt >= capacity)
+			throw new OverflowIntStackException();
+		data[pt++] = p;
+		return;
+	}
+
+	public Point pop() throws EmptyIntStackException {
+		if (pt <= 0)
+			throw new EmptyIntStackException();
+		return data[--pt];
+	}
+
+	public Point peek() throws EmptyIntStackException {
+		if (pt <= 0)
+			throw new EmptyIntStackException();
+		return data[pt - 1];
+	}
+
+	public void clear() {
+		pt = 0;
+	}
+
+	public int indexOf(Point p) {
+		for (int i = 0; i < data.length; i++) {
+			if (data[i].equals(p))
+				return i;
+		}
+		return -1;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public int size() {
+		return pt;
+	}
+
+	public boolean isEmpty() {
+		return pt <= 0;
+	}
+
+	public boolean isFull() {
+		return pt >= capacity;
+	}
+
+	public void dump() {
+		if (pt <= 0)
+			System.out.println("스택이 비어 있습니다.");
+		else {
+			for (int i = 0; i < pt; i++) {
+				System.out.print(data[i] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+public class Chap5_Test_QueenEight_4회차_01 {
+
+	public static boolean checkRow(int[][] d, int x) {
+		for (int i = 0; i < d[0].length; i++) {
+			if (d[x][i] == 1)
+				return false;
+		}
+		return true;
+	}
+
+	public static boolean checkCol(int[][] d, int y) {
+		for (int i = 0; i < d.length; i++) {
+			if (d[i][y] == 1)
+				return false;
+		}
+		return true;
+	}
+
+	public static boolean checkSE(int[][] d, int x, int y) {
+		for (int ix = x, iy = y; ix < d.length && iy < d[0].length; ix++, iy++) {
+			if (d[ix][iy] == 1)
+				return false;
+		}
+		for (int ix = x, iy = y; ix >= 0 && iy >= 0; ix--, iy--) {
+			if (d[ix][iy] == 1)
+				return false;
+		}
+		return true;
+	}
+
+	public static boolean checkSW(int[][] d, int x, int y) {
+		for (int ix = x, iy = y; ix < d.length && iy >= 0; ix++, iy--) {
+			if (d[ix][iy] == 1)
+				return false;
+		}
+		for (int ix = x, iy = y; ix >= 0 && iy < d[0].length; ix--, iy++) {
+			if (d[ix][iy] == 1)
+				return false;
+		}
+		return true;
+	}
+
+	public static boolean checkMove(int[][] d, int x, int y) {
+		return checkRow(d, x) && checkCol(d, y) && checkSE(d, x, y) && checkSW(d, x, y);
+	}
+
+	public static Point nextMove(int[][] d, int x, int y) {
+		for (int i = y; i < d[0].length; i++) {
+			if (checkMove(d, x, i)) {
+				Point p = new Point(x, i);
+				return p;
+			}
+		}
+		return null;
+	}
+
+	public static void solveQueen(int[][] d) {
+		int n = 8;
+		int count = 0;
+		int x = 0, y = 0;
+		Stack s = new Stack(10);
+		d[x++][y] = 1;
+		count++;
+		Point p = new Point(x, y);
+		s.push(p);
+
+		while (count < n) {
+			p = nextMove(d, x, y);
+			if (p != null) {
+				x = p.getX();
+				y = p.getY();
+				d[x++][y] = 1;
+				count++;
+				s.push(p);
+				y = 0;
+			} else {
+				if (count != 0) {
+					p = s.pop();
+					x = p.getX();
+					y = p.getY();
+					d[x][y] = 0;
+					count--;
+					y++;
+				} else {
+					break;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int row = 8, col = 8;
+		int[][] data = new int[row][col];
+		for (int i = 0; i < data.length; i++)
+			for (int j = 0; j < data[0].length; j++)
+				data[i][j] = 0;
+
+		solveQueen(data);
+
+		for (int[] element : data) {
+			for (int j = 0; j < data[0].length; j++) {
+				System.out.print(element[j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
